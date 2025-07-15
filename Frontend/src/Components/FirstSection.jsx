1a11cec0cfdf22d4fb0cc51814efb978d71c029d
Vedant5125 <vedantapraj5125@gmail.com>
2025-07-15 21:01:55 +0530

    Fix: cleaned merge conflict and corrected Project.jsx

--- Frontend/src/Components/FirstSection.jsx
+++ Frontend/src/Components/FirstSection.jsx
@@ -5,7 +5,7 @@ import "./FirstSection.css";
 const FirstSection = () => {
 
 const handleDownload = () => {
-  window.open('https://drive.google.com/file/d/1o4v4x49U6_aAY0ZnhgTXmagKbaKTtG21/view?usp=sharing', '_blank');
+  window.open('https://drive.google.com/file/d/1o4v4x49U6_aAY0ZnhgTXmagKbaKTtG21/view?usp=sharing');
 };
 
 
